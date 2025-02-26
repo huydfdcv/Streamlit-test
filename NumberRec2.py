6c925bb793b86a13d209a4e68385fa7ac8e4e91d
huydfdcv <huydfdcv@gmail.com>
2025-02-27 01:04:53 +0700

    Update NumberRec2.py

--- NumberRec2.py
+++ NumberRec2.py
@@ -3,25 +3,22 @@ import mlflow
 import mlflow.sklearn
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 from mlflow.models import infer_signature
-from mlflow.tracking import MlflowClient
 from PIL import Image
 from streamlit_drawable_canvas import st_canvas
-
+import os
 # Cấu hình MLflow
-mlflow.set_tracking_uri("https://dagshub.com/huydfdcv/my-first-repo.mlflow")
+MLFLOW_TRACKING_URI = "https://dagshub.com/huydfdcv/my-first-repo.mlflow"
+mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 mlflow.set_experiment("MNIST_Classification")
+os.environ["MLFLOW_TRACKING_USERNAME"] = "MLFLOW_TRACKING_USERNAME"
+os.environ["MLFLOW_TRACKING_PASSWORD"] = "MLFLOW_TRACKING_PASSWORD"
 
-mlflow.login(
-    username=st.secrets["MLFLOW_TRACKING_USERNAME"],
-    password=st.secrets["MLFLOW_TRACKING_PASSWORD"]
-)
 st.title("MNIST Classification & Clustering with Streamlit & MLFlow")
 
 # 1. Thu thập dữ liệu
